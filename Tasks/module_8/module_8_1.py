70f6edc606b6231f9149ae805993bb2918bb14e2
Vladislav <metadonkis@gmail.com>
2024-12-03 16:27:29 +0300

    Delete Tasks directory

--- Tasks/module_8/module_8_1.py
+++
@@ -1,8 +0,0 @@
-def add_everything_up(a, b):
-     try:
-        return a + b
-     except TypeError:
-         return str(a) + str(b)
-
-print(add_everything_up(17, 'ghbd'))
-print(add_everything_up(17, 42))
