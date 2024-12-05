56193d77f2c2b3a25af99bacfd3f910a28103aa3
Vladislav <metadonkis@gmail.com>
2024-12-05 13:52:13 +0300

    Update module_9_2.py

--- module_09/module_9_2.py
+++ module_09/module_9_2.py
@@ -2,10 +2,12 @@ first_strings = ["Elon", "Musk", "Programmer", "Monitors", "Variable"]
 second_strings = ["Task", "Git", "Comprehension", "Java", "Computer", "Assembler"]
 
 firs_result = [len(i) for i in first_strings if len(i) > 5] # 1 Пункт
-second_result = [x + y for x in first_strings for y in second_strings if len(x) == len(y)]
-third_result = {x: len(x) for x in [*first_strings, *second_strings]}
-third_result = [i for i in third_result.items()]
+
+second_result = [x + y for x in first_strings for y in second_strings if len(x) == len(y)] # 2 Пункт
+
+third_result = {x: len(x) for x in [*first_strings, *second_strings] if len(x) % 2 == 0} # 3 Пункт
+
 
 print(firs_result, '\n')
 print(second_result, '\n')
-print(*third_result, sep='\n')
+print(third_result, sep='\n')
