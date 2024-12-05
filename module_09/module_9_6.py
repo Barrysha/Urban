c541cf8770e65c176037843b669ef05fe0474232
Vladislav <metadonkis@gmail.com>
2024-12-05 16:19:40 +0300

    Update module_9_6.py

--- module_09/module_9_6.py
+++ module_09/module_9_6.py
@@ -1,10 +1,18 @@
 def all_var(text):
+    
+    for i in range(len(text)):
+        len_substring = i + 1  # длина подпоследовательности
+        pointer = 0
+        while len(text) - pointer > 0:
+            if len(text[pointer: pointer + len_substring]) == len_substring:
+                yield text[pointer: pointer + len_substring]
+            else:
+                break
+            pointer += 1
 
-    strings = ''
-    for i in text:
-        strings += i
-        yield strings
 
-a = all_var("ABC")
-for i in a:
-    print(i)
+
+if __name__ == "__main__":
+    a = all_var("abc")
+    for i in a:
+        print(i)
